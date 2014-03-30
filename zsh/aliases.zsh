4000a8b76f9fe7f672b103779c76cd96490fdf10
Jordan Garcia <jordan@Jordans-MacBook-Pro.local>
2014-03-29 23:29:44 -0700

    Strip down vimrc and consolidate

---
+++ zsh/aliases.zsh
@@ -0,0 +1,20 @@
+alias reload!='. ~/.zshrc'
+
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+if which gls &>/dev/null
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+else
+  alias ls="ls"
+  alias l="ls -al"
+  alias ll="ls -l"
+fi
+
+alias genctags='ctags -R --sort=yes --verbose --languages=+PHP,-JavaScript --exclude=@$HOME/.ctags.exclude'
+alias vim='TERM=xterm-256color vim'
+~
