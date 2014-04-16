591f0586a95b10deeb349fc6e3bd92a292a29634
jordangarcia <jordan.garcia@gmail.com>
2014-04-16 11:23:34 -0700

    Add optimizely/out to PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/bin:$ZSH/bin:$PATH"
+export PATH="/Users/jordan/code/optimizely/out:/usr/local/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
